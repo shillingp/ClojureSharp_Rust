1fe428447bc728e5853e557330e74a7051a0807e
Peter Shilling <p.shilling.ps@gmail.com>
2025-01-03 16:20:38 +0000

    improve error handling

--- src/main.rs
+++ src/main.rs
@@ -15,16 +15,11 @@ fn main() {
     let contents: String =
         fs::read_to_string(input_file_path).expect("Should have been able to read the file");
 
-    let source_code_tokens: Vec<Token> = match tokenizer::tokenizer::tokenize(contents) {
-        Ok(tokens) => tokens,
-        Err(error) => panic!("Failed to tokenize source code\n{}", error),
-    };
+    let source_code_tokens: Vec<Token> = tokenizer::tokenizer::tokenize(contents)
+        .expect("Failed to tokenize source code");
 
-    let abstract_syntax_tree: SyntaxTreeNode =
-        match syntax_tree::syntax_tree_builder::parse(source_code_tokens) {
-            Ok(tree) => tree,
-            Err(error) => panic!("Failed to parse abstract syntax tree\n{}", error),
-        };
+    let abstract_syntax_tree: SyntaxTreeNode = syntax_tree::syntax_tree_builder::parse(source_code_tokens)
+        .expect("Failed to parse abstract syntax tree");
 
     let transpiled_code: String = transpiler::transpiler::transpile(abstract_syntax_tree);
 
@@ -32,6 +27,12 @@ fn main() {
     let pretty_transpiled_code: String = prettifier.prettify(transpiled_code);
 
     let output_file_path: &Path = Path::new("./src/output/result.clj");
+
+    if let Some(directory_path) = output_file_path.parent() {
+        fs::create_dir_all(directory_path)
+            .expect("Should have been able to create the directory");
+    }
+
     fs::write(output_file_path, pretty_transpiled_code)
         .expect("Should have been able to write output file");
 }
